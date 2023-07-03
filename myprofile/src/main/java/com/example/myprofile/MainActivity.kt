781a9f19a37aa8b81465075308156a83260ed9de
dongmin0421 <brian050212@gmail.com>
2023-07-03 10:53:31 +0900

    profile

--- myprofile/src/main/java/com/example/myprofile/MainActivity.kt
+++ myprofile/src/main/java/com/example/myprofile/MainActivity.kt
@@ -15,17 +15,7 @@ import com.example.myprofile.ui.theme.AndKotTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            AndKotTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
-        }
+        setContentView(R.layout.activity_main)
     }
 }
 
